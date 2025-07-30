f538a2746f49ea63b044489550ae758792944600
Vasile <lixcode@vivaldi.net>
2025-07-30 13:35:27 +0300

    Update library version to 3.1.1

--- babel.config.cjs
+++ babel.config.cjs
@@ -2,7 +2,11 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = [];
-  const plugins = ["@babel/plugin-syntax-jsx", "vasille", ["@babel/plugin-transform-typescript", {isTSX: true}]];
+  const plugins = [
+    "@babel/plugin-syntax-jsx",
+    ["vasille", {devMode: process.env.NODE_ENV !== "production"}],
+    ["@babel/plugin-transform-typescript", {isTSX: true}]
+  ];
 
   return {
     presets,
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,77 @@
-import { Debug, compose } from "vasille-web";
+import {
+  compose,
+  styleSheet,
+  prefersLight,
+  prefersDark,
+  dark,
+  laptop,
+  tablet,
+  mobile,
+  value,
+  theme
+} from "vasille-web";
+
 
 export const App = compose(() => {
-  <div class="example">
-    Welcome to Vasille example
-    <Debug model="Look to me from dev tools"/>
-  </div>
+  let dark = false;
+  let currentTheme = value("red");
+
+  document.body.classList.add(currentTheme);
+
+  function switchDark() {
+    dark = !dark;
+  }
+  function setTheme(theme: string) {
+    if (theme === currentTheme) {
+      return;
+    }
+    document.body.classList.remove(currentTheme);
+    document.body.classList.add(theme);
+    currentTheme = theme;
+  }
+
+  <div>
+    <button class={[styles.auto]}>Auto dark by user preferences</button>
+  </div>;
+  <div class={[dark && "dark"]}>
+    <button class={[styles.manual]} onclick={switchDark}>Switch dark mode</button>
+  </div>;
+  <div class={[styles.responsive]}>
+    <p>I'm responsive</p>
+  </div>;
+  <div>
+    <button class={[styles.themed]} onclick={() => setTheme("red")}>Set red theme</button>
+    <button class={[styles.themed]} onclick={() => setTheme("green")}>Set green theme</button>
+    <button class={[styles.themed]} onclick={() => setTheme("blue")}>Set blue theme</button>
+  </div>;
 });
+
+const styles = styleSheet({
+  auto: {
+    "background-color": [prefersLight("#fff"), prefersDark("#111")],
+    "color": [prefersLight("#111"), prefersDark("#fff")],
+    ":hover": {
+      "background-color": ["#eee", prefersDark('#222')],
+    }
+  },
+  manual: {
+    "background-color": ["#fff", dark("#111")],
+    "color": ["#111", dark("#fff")],
+  },
+  responsive: {
+    "width": [1000, laptop("50%"), tablet("75%"), mobile("100%")],
+    "height": [100, tablet(mobile(50))],
+    "background-color": "#adadad",
+    "margin-left": 0,
+    "margin-right": 0,
+  },
+  themed: {
+    "border-width": 2,
+    "border-color": [
+      theme("red", "#f00"),
+      theme("green", "#0f0"),
+      theme("blue", "#00f")
+    ],
+    "margin": 10,
+  }
+})
